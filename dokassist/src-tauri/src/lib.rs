bd5e0f39908e35598013704f67d026f9ab307824
vgsteiger <v.gsteiger@gmail.com>
2026-08-06 09:00:08 +0200

    Scope frontend filesystem writes

--- dokassist/src-tauri/src/lib.rs
+++ dokassist/src-tauri/src/lib.rs
@@ -43,7 +43,6 @@ pub fn run() {
     let app_state = AppState::new(data_dir);
 
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
